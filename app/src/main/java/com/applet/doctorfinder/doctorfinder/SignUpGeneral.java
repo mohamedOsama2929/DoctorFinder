a1eef6ef5644e0dd46ecee240c0b104336aad6ab
amran <amranwar797@gmail.com>
2017-09-30 23:04:57 +0200

    amr

--- app/src/main/java/com/applet/doctorfinder/doctorfinder/SignUpGeneral.java
+++ app/src/main/java/com/applet/doctorfinder/doctorfinder/SignUpGeneral.java
@@ -8,10 +8,8 @@ import android.view.View;
 public class SignUpGeneral extends AppCompatActivity {
 
     public void Go(View view){
-
         Intent intent = new Intent(this, SignUpAsDoctor.class);
         startActivity(intent);
-
     }
 
     @Override
