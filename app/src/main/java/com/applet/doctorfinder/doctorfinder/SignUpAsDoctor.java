82d545f7abedb1e52c7d0af3786de5935a131a19
amran <amranwar797@gmail.com>
2017-09-30 23:02:06 +0200

    amr

---
+++ app/src/main/java/com/applet/doctorfinder/doctorfinder/SignUpAsDoctor.java
@@ -0,0 +1,13 @@
+package com.applet.doctorfinder.doctorfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpAsDoctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_as_doctor);
+    }
+}
---
+++ app/src/main/java/com/applet/doctorfinder/doctorfinder/SignUpGeneral.java
@@ -0,0 +1,22 @@
+package com.applet.doctorfinder.doctorfinder;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpGeneral extends AppCompatActivity {
+
+    public void Go(View view){
+
+        Intent intent = new Intent(this, SignUpAsDoctor.class);
+        startActivity(intent);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_general);
+    }
+}
