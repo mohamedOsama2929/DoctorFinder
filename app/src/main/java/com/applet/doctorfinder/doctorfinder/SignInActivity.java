83be4ef3b721dc47cb7e4dbc469328ee9d410ecf
ibrahimAbdelKarim <ibr29199@gmail.com>
2017-10-10 19:46:56 +0200

    home activity added

--- app/src/main/java/com/applet/doctorfinder/doctorfinder/SignInActivity.java
+++ app/src/main/java/com/applet/doctorfinder/doctorfinder/SignInActivity.java
@@ -1,7 +1,12 @@
 package com.applet.doctorfinder.doctorfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_signin)
+    public void signIn(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
