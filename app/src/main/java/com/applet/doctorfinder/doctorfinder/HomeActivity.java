1751a5b797976695ab3fcdaf4f1432babfe8babd
osama <monkosama1996@gmail.com>
2017-10-11 19:27:11 +0200

    navdone

--- app/src/main/java/com/applet/doctorfinder/doctorfinder/HomeActivity.java
+++ app/src/main/java/com/applet/doctorfinder/doctorfinder/HomeActivity.java
@@ -5,7 +5,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 
 public class HomeActivity extends ActionBarActivity {
 
@@ -18,6 +17,7 @@ public class HomeActivity extends ActionBarActivity {
 
          toolbar=(Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
         //for toolbar button to open nav drawer
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
