b8f5aba8fe923db5910cfc388311b2562eb57437
osama <monkosama1996@gmail.com>
2017-10-15 15:56:20 +0200

    searchResultAdded

--- app/src/main/java/com/applet/doctorfinder/doctorfinder/view/HomeActivity.java
+++ app/src/main/java/com/applet/doctorfinder/doctorfinder/view/HomeActivity.java
@@ -3,11 +3,12 @@ package com.applet.doctorfinder.doctorfinder.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBarDrawerToggle;
+
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import com.applet.doctorfinder.doctorfinder.R;
 
@@ -19,6 +20,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitymain_appbar);
+        ButterKnife.bind(this);
 
       //   toolbar=(Toolbar)findViewById(R.id.app_bar);
        // setSupportActionBar(toolbar);
@@ -36,6 +38,12 @@ public class HomeActivity extends AppCompatActivity {
         //getSupportActionBar().setDisplayShowHomeEnabled(true);
         navFragment drawerFragment=(navFragment)getSupportFragmentManager().findFragmentById(R.id.draw);
         drawerFragment.setUP((DrawerLayout)findViewById(R.id.drawer_layout));
+    }
+    @OnClick(R.id.btn_findDoctor)
+    public void searchResult(View view) {
+        Intent intent = new Intent(this,SearchResults.class);
+        startActivity(intent);
+
     }
 
 }
