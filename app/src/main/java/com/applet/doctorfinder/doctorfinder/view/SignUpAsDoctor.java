1325bab52a8c83a253a610c2f7b33d9c5a664f31
amran <amranwar797@gmail.com>
2017-10-19 10:31:13 +0200

    AmrFindDoc

--- app/src/main/java/com/applet/doctorfinder/doctorfinder/view/SignUpAsDoctor.java
+++ app/src/main/java/com/applet/doctorfinder/doctorfinder/view/SignUpAsDoctor.java
@@ -1,15 +1,75 @@
 package com.applet.doctorfinder.doctorfinder.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 
 import com.applet.doctorfinder.doctorfinder.R;
 
+import butterknife.OnClick;
+
 public class SignUpAsDoctor extends AppCompatActivity {
+
+    String[] languages = { "C","C++","Java","C#","PHP","JavaScript","jQuery","AJAX","JSON" };
+
+    public void back(View view){
+
+        Intent intent = new Intent(this,SignUpGeneral.class);
+        startActivity(intent);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_as_doctor);
+
+        final AutoCompleteTextView doctorSpeciality = (AutoCompleteTextView) findViewById(R.id.doctorSpeciality);
+        final AutoCompleteTextView doctorSubSpeciality = (AutoCompleteTextView) findViewById(R.id.doctorSubSpeciality);
+        final AutoCompleteTextView doctorCity = (AutoCompleteTextView) findViewById(R.id.doctorCity);
+        final AutoCompleteTextView doctorArea = (AutoCompleteTextView) findViewById(R.id.doctorArea);
+
+        ArrayAdapter<String> doctorSpecialityadapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line, languages);
+        ArrayAdapter<String> doctorSubSpecialityadapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line, languages);
+        ArrayAdapter<String> doctorCityadapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line, languages);
+        ArrayAdapter<String> doctorAreaadapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line, languages);
+
+        doctorSpeciality.setAdapter(doctorSpecialityadapter);
+        doctorSubSpeciality.setAdapter(doctorSubSpecialityadapter);
+        doctorCity.setAdapter(doctorCityadapter);
+        doctorArea.setAdapter(doctorAreaadapter);
+
+        findViewById(R.id.doctorSpeciality).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doctorSpeciality.showDropDown();
+            }
+        });
+
+        findViewById(R.id.doctorSubSpeciality).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doctorSubSpeciality.showDropDown();
+            }
+        });
+
+        findViewById(R.id.doctorCity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doctorCity.showDropDown();
+            }
+        });
+
+        findViewById(R.id.doctorArea).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doctorArea.showDropDown();
+            }
+        });
+
     }
 }
