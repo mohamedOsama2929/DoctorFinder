fb964f1718c42e341cae3e9e80ea2efc2ff8a033
amran <amranwar797@gmail.com>
2017-10-20 19:08:39 +0200

    AmrFindDoc

--- app/src/main/java/com/applet/doctorfinder/doctorfinder/view/SearchResults.java
+++ app/src/main/java/com/applet/doctorfinder/doctorfinder/view/SearchResults.java
@@ -1,18 +1,29 @@
 package com.applet.doctorfinder.doctorfinder.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.applet.doctorfinder.doctorfinder.R;
+
+import butterknife.OnClick;
 
 public class SearchResults extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    public void back(View view){
+
+        Intent intent = new Intent(this,HomeActivity.class);
+        startActivity(intent);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
